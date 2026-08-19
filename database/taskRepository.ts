dc174629ac26f414168bde620ace961025077cf2
StreetFly <ryannitroo@gmail.com>
2026-08-18 22:04:08 -0700

    simplified database connection, added createTask for sql

--- database/taskRepository.ts
+++ database/taskRepository.ts
@@ -7,30 +7,29 @@ export type Task = {
     id: number;
     title: string;
     completed: number;
-    createdAt : string;
+    createdAt: string;
 };
-const db = SQLite.openDatabaseAsync(DATABASE_NAME);
+const db = await SQLite.openDatabaseAsync(DATABASE_NAME);
 
 export async function initializeDatabase() {
-  const database = await db;
-
-  await database.execAsync(`
+  await db.execAsync(`
     CREATE TABLE IF NOT EXISTS metadata (
       key TEXT PRIMARY KEY NOT NULL,
       value INTEGER NOT NULL
     );
   `);
 
-  const version = await database.getFirstAsync<{ value: number }>(
+  const version = await db.getFirstAsync<{ value: number }>(
     `SELECT value 
     FROM metadata 
-    WHERE key = ?`,'schema_version'
+    WHERE key = ?`,
+    'schema_version'
   );
 
   const currentVersion = version?.value ?? 0;
 
   if (currentVersion < 1) {
-    await database.execAsync(`
+    await db.execAsync(`
       CREATE TABLE IF NOT EXISTS tasks (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         title TEXT NOT NULL,
@@ -38,15 +37,27 @@ export async function initializeDatabase() {
         createdAt TEXT NOT NULL
       );
 
-      CREATE INDEX IF NOT EXISTS idx_tasks_createdAt
-      ON tasks(createdAt);
+      CREATE INDEX IF NOT EXISTS idx_tasks_completed
+      ON tasks(completed);
     `);
 
-    await database.runAsync(
+    await db.runAsync(
       `INSERT OR REPLACE INTO metadata (key, value)
        VALUES (?, ?)`,
       'schema_version',
       DATABASE_VERSION
     );
   }
+}
+
+export async function createTask(title: string) {
+    const createdAt = new Date().toString();
+
+    const result = await db.runAsync(
+        `INSERT INTO tasks (title, completed, createdAt)
+        VALUES (?, ?, ?)`,
+        title, 0, createdAt
+    );
+
+    return result.lastInsertRowId
 }
